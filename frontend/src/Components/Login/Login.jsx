130d63831e3a857f7a988320c3568147036dd283
Ranjan Vaidya <ranjanvaidya47@gmail.com>
2024-09-08 11:01:15 +0530

    ui updates

--- frontend/src/Components/Login/Login.jsx
+++ frontend/src/Components/Login/Login.jsx
@@ -33,7 +33,7 @@ const Login = () =>{
     <div className="login">
       <form className="loginForm" onSubmit={loginHandler}>
         <Typography variant="h3" style={{ padding: "2vmax" }}>
-          Social App
+          InstaMedia
         </Typography>
 
         <input
